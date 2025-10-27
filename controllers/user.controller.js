07f7813b4cad61b3458a0f2d65d8dba3df73c772
unknown <nafistahmid4@gmail.com>
2025-10-27 16:04:22 +0600

    Cookies code updated for backend commit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -89,7 +89,8 @@ export const login = async (req, res) => {
     return res
       .cookie("token", token, {
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "None",
+        secure: false,
         maxAge: 1 * 24 * 60 * 60 * 1000,
       })
       .json({
